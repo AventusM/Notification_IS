20b3f16fa0cedb1e0155d86b9e8ffa152ee612f3
Anton <anton.moroz@productivityleap.com>
2023-03-06 15:41:21 +0200

    Adding cors

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
-app.get("/api/status", (_req, res) => {
+app.get("/", (_req, res) => {
   res.send({ status: "error", message: "An api error message" });
 });
 
